78bf8fbe39a6510963a4c5686493d6ad2b0d76bd
Alexander Sulfrian <alexander@sulfrian.net>
2009-12-25 20:53:04 +0100

    converted UMenuBackgrund

---
+++ src/menu/menuBackground.hpp
@@ -0,0 +1,52 @@
+/*
+ * UltraStar Deluxe - Karaoke Game
+ *
+ * UltraStar Deluxe is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * $URL$
+ * $Id$
+ */
+
+#ifndef MENUBACKGROUND_HPP
+#define MENUBACKGROUND_HPP
+
+namespace usdx
+{
+	// MenuBackground - abstract class for MenuBackgrounds
+	class MenuBackground
+	{
+	public:
+		// all members are pure virtual
+		virtual MenuBackground(const ThemeBeckground* themed_settings) = 0;
+		virtual ~MenuBackground() = 0;
+
+		virtual void on_show(void) = 0;
+		virtual void on_finish(void) = 0;
+
+		virtual void draw(void) = 0;
+
+	protected:
+		// no copy and no assignment
+		MenuBackground(const MenuBackground&);
+		MenuBackground& operator=(const MenuBackground&);
+	};
+};
+
+#endif
