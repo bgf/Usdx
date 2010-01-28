bf51258e9f9a5587f7307de5d77aad41d6312d70
Alexander Sulfrian <alexander@sulfrian.net>
2010-01-28 13:40:14 +0100

    added language class for easy translation

---
+++ src/base/language.cpp
@@ -0,0 +1,128 @@
+/*
+ * UltraStar Deluxe - Karaoke Game
+ *
+ * UltraStar Deluxe is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * $URL$
+ * $Id$
+ */
+
+#include <boost/filesystem.hpp>
+#include "language.hpp"
+
+// namespace alias for shorter class names
+namespace fs = boost::filesystem;
+
+namespace usdx
+{
+	Language* Language::instance = NULL;
+
+	LanguageNotFound::LanguageNotFound(const std::wstring& language) :
+		BaseException("Language not found!"),
+		language(language)
+	{
+	}
+
+	const std::wstring& LanguageNotFound::get_language() const
+	{
+		return language;
+	}
+
+
+	Language::Language(void)
+	{
+	}
+
+	Language::~Language(void)
+	{
+	}
+
+	Language* Language::get_instance(void)
+	{
+		if (instance == NULL) {
+			instance = new Language();
+		}
+
+		return instance;
+	}
+
+	void Language::load_language(const std::wstring& language)
+	{
+		// TODO
+	}
+
+	void Language::init(const fs::wpath& language_dir)
+	{
+		if (!is_directory(language_dir)) {
+			return;
+		}
+
+		this->language_dir = language_dir;
+
+		// default construction yields past-the-end
+		fs::wdirectory_iterator end_it;
+		for (fs::wdirectory_iterator it(language_dir); it != end_it; ++it)
+		{
+			if (!is_directory(it->status()))
+			{
+				if (extension(it->path()) == L".ini")
+				{
+					translations[basename(it->path())] = NULL;
+				}
+			}
+		}
+	}
+
+	void Language::set_language(const std::wstring& language)
+	{
+		std::map<std::wstring, std::map<std::wstring, std::wstring>*>::iterator it = translations.find(language);
+		if (it == translations.end()) {
+			throw LanguageNotFound(language);
+		}
+
+		if (it->second == NULL) {
+			load_language(language);
+		}
+
+		current_language_name = std::wstring(language);
+		current_language = translations[current_language_name];
+	}
+
+	const std::wstring& Language::get_language(void) const
+	{
+		return current_language_name;
+	}
+
+	const std::wstring& Language::translate(const std::wstring& source) const
+	{
+
+		std::map<std::wstring, std::wstring>::iterator it = current_language->find(source);
+		if (it != current_language->end()) {
+			return it->second;
+		}
+
+		it = default_language->find(source);
+		if (it != default_language->end()) {
+			return it->second;
+		}
+
+		return source;
+	}
+};
---
+++ src/base/language.hpp
@@ -0,0 +1,84 @@
+/*
+ * UltraStar Deluxe - Karaoke Game
+ *
+ * UltraStar Deluxe is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * $URL$
+ * $Id$
+ */
+
+#ifndef LANGUAGE_HPP
+#define LANGUAGE_HPP
+
+#include <map>
+#include <string>
+#include <boost/filesystem.hpp>
+#include "utils/base_exception.hpp"
+
+/**
+ * Helper macro for translating strings with a short pseudo function call.
+ */
+#define _(x) Language::get_instance()->translate(x)
+
+namespace usdx
+{
+	class LanguageNotFound : public BaseException
+	{
+	private:
+		std::wstring language;
+	public:
+		LanguageNotFound(const std::wstring& language);
+		~LanguageNotFound () throw () {};
+
+		const std::wstring& get_language() const;
+	};
+
+	class Language
+	{
+	private:
+		boost::filesystem::wpath language_dir;
+
+		std::map<std::wstring, std::wstring>* current_language;
+		std::wstring current_language_name;
+
+		std::map<std::wstring, std::wstring>* default_language;
+
+		std::map<std::wstring, std::map<std::wstring, std::wstring>*> translations;
+
+		void load_language(const std::wstring& language);
+
+		Language();
+
+		static Language* instance;
+	public:
+		virtual ~Language();
+
+		Language* get_instance();
+
+		void init(const boost::filesystem::wpath& language_dir);
+
+		void set_language(const std::wstring& language);
+		const std::wstring& get_language(void) const;
+
+		const std::wstring& translate(const std::wstring& source) const;
+	};
+};
+
+#endif
